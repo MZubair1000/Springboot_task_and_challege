3098ca3bb74375396a4a2d5c8a2e1eb8d1cd2187
Zubicodes1 <97788998+Zubicodes1@users.noreply.github.com>
2023-02-19 09:05:11 +0500

    Task and challenge files are added into the master branch

---
+++ producerAndConsumerChallenge/src/test/java/com/example/producerAndConsumerChallenge/ProducerAndConsumerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producerAndConsumerChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerAndConsumerChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
